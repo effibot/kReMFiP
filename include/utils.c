dcf0c8bee826432a9831da64abd8a4ea8a8f8b8b
Andrea Efficace <andrea.efficace1@gmail.com>
2024-08-03 00:15:27 +0200

    update

---
+++ include/utils.c
@@ -0,0 +1,37 @@
+#include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/errno.h>
+#include <linux/device.h>
+#include <linux/kprobes.h>
+#include <linux/mutex.h>
+#include <linux/mm.h>
+#include <linux/sched.h>
+#include <linux/slab.h>
+#include <linux/version.h>
+#include <linux/interrupt.h>
+#include <linux/time.h>
+#include <linux/string.h>
+#include <linux/vmalloc.h>
+#include <linux/syscalls.h>
+#include <linux/compiler.h>
+#include <linux/spinlock.h>
+#include <asm/page.h>
+#include <asm/cacheflush.h>
+#include <asm/apic.h>
+#include <asm/unistd_64.h>
+
+#include "types.h"
+#include "utils.h"
+
+static rm_t *rm_init(void){
+	rm_t *rm = (rm_t *) kmalloc(sizeof(rm_t), GFP_KERNEL);
+	rm->allowed_modes = al_mode_t;
+	rm->blocked_modes = 0;
+	rm->hooked_functions = hooked_functions;
+	return rm;
+}
+
+static int rm_free(rm_t *rm){
+    return kfree(rm);
+}
---
+++ include/utils.h
@@ -0,0 +1,38 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <linux/version.h>
+
+#include "types.h"
+
+
+// Define a macro to get the uid/euid of the current process
+#define get_uid()              current->cred->uid.val
+#define get_euid()             current->cred->euid.val
+// Define a macro to set the uid/euid of the current process
+#define set_uid(uid)           current->cred->uid.val = uid
+#define set_euid(euid)         current->cred->euid.val = euid
+// Define a macro to get the pid of the current process
+#define get_pid()              current->pid
+
+
+// Define a macro to get major and minor numbers of a device file
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 0, 0)
+#define get_major(session)      MAJOR(session->f_inode->i_rdev)
+#define get_minor(session)      MINOR(session->f_inode->i_rdev)
+#else
+#define get_major(session)      MAJOR(session->f_dentry->d_inode->i_rdev)
+#define get_minor(session)      MINOR(session->f_dentry->d_inode->i_rdev)
+#endif
+
+
+
+// Initialize a reference monitor structure
+
+static rm_t *rm_init(void);
+
+
+// Free a reference monitor structure
+static int rm_free(rm_t *rm);
+
+#endif
